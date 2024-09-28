8f27b046e67246db66eaa8b95c6736b9d65110cb
adeelfeb <at4563323@gmail.com>
2024-09-28 21:14:55 +0500

    ok here is a scary thing, stackblitz maybe not keeping track of all the things
    need to commit them else they get lost 
    or maybe i'm tripping since all the codes are stuffed in a single place

--- Frontend/Learning/HookRevision/hook/src/App.jsx
+++ Frontend/Learning/HookRevision/hook/src/App.jsx
@@ -6,10 +6,27 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const Increment = () => {
+    if (count !== 20) {
+      setCount(count + 1)
+    }
+  }
+
+  const Decrement = () => {
+    if (count !== 0) {
+      setCount(count - 1)
+    }
+  }
+
   return (
-    <>
-      <h1>Getting started again here !</h1>
-    </>
+    <div className='bg-black'>
+      <h1>Getting started again here!</h1>
+      <h2>Counter is at: {count}</h2>
+      <button onClick={Increment}>
+        Increase
+      </button>
+      <button onClick={Decrement}>Decrease</button>
+    </div>
   )
 }
 
