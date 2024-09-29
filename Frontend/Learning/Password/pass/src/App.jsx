200b253a0e2b7fdcdd4a34dda28080fb6bee2a2e
adeelfeb <at4563323@gmail.com>
2024-09-29 20:33:29 +0500

    ok there was issue with the code this time
    you know that's how we learng thingiss mate

--- Frontend/Learning/Password/pass/src/App.jsx
+++ Frontend/Learning/Password/pass/src/App.jsx
@@ -1,43 +1,38 @@
 import { useState, useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [length, setlength] = useState(8)
+  const [length, setLength] = useState(8)
   const [number, setNumber] = useState(false)
   const [character, setCharacter] = useState(false)
-  cosnt [password, setPassword] = useState("")
-  const GeneratePassword = useCallback(()=>{
+  const [password, setPassword] = useState("")
+
+  const GeneratePassword = useCallback(() => {
     let pass = ""
     let strn = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if(number) strn+= "0123456789"
-    if(character) strn+= "!@#$%^&*()_-+={}[]|\:;'<>,.?/"
+    if (number) strn += "0123456789"
+    if (character) strn += "!@#$%^&*()_-+={}[]|\\:;'<>,.?/"
 
     for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * strn.length +1 )
-      pass = strn.charAt(char)
+      let char = Math.floor(Math.random() * strn.length)
+      pass += strn.charAt(char)
     }
     setPassword(pass)
-
-
-  },[length,number,password,character])
-  
-
+  }, [length, number, character])
 
   return (
     <>
-      <div className='bg-black min-h-screen'>
-        <h1 className='text-4xl text-center text-black'>Password Generator</h1>
-      </div>
-      <div className='bg-black min-h-screen'>
+    
+      <div className='bg-black min-h-screen p-9 m-4 rounded-xl'>
         <div className='rounded-full w-full max-w-md mx-auto shadow-md px-4 my-8 text-orange-600
         bg-gray-700'>
           testing
         </div>
       </div>
+      {/* <div className='bg-black min-h-screen'>
+        <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      </div> */}
     </>
-
   )
 }
 
