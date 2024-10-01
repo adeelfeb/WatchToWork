d464df5be7d1716cf6f06e6f8e58c2432e25a069
adeelfeb <at4563323@gmail.com>
2024-10-01 17:52:26 +0500

    lets just commit here since it will not store the work done uptill this point

--- Frontend/custom/src/App.jsx
+++ Frontend/custom/src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { InputBox } from './Components'
+import useCurrencyInfo from './hook/useCurrencyInfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState('usd')
+  const [to, setTo] = useState('pkr')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyObject = useCurrencyInfo(from)
+  const currencyKeys = Object.keys(currencyObject)
+
+  const swap = ()=>{
+    setFrom(to)
+    setTo(from)
+    setAmount(convertedAmount)
+    setConvertedAmount(amount)
+
+    const convert = ()=>{
+      setConvertedAmount(amount * currencyObject[to])
+    }
+  }
 
   return (
     <>
