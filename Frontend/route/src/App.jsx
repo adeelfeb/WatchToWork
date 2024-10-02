e6297a9fd64c08c6b590bdaae799cc0a31e13ad0
adeelfeb <at4563323@gmail.com>
2024-10-02 18:17:59 +0500

    starting with routers in React

---
+++ Frontend/route/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <button className='p-9 bg-black text-white'>Hi there testing</button>
+    </>
+  )
+}
+
+export default App
