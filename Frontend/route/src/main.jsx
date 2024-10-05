ddf1d9da3c3f827768d9a91b4ce2d0b5b48d3161
adeelfeb <at4563323@gmail.com>
2024-10-05 22:45:47 +0500

    ok another method to add routers

--- Frontend/route/src/main.jsx
+++ Frontend/route/src/main.jsx
@@ -2,27 +2,44 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './Components/Home/Home.jsx'
 import About from './Components/About/About.jsx'
+import Contact from './Components/Contact/Contact.jsx'
 
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout/>,
-    children:[{
-      path:"",
-      element:<Home/>
-    },
-    {
-      path:"",
-      element:<About/>
-    }
-  ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children:[{
+//       path:"",
+//       element:<Home/>
+//     },
+//     {
+//       path:"/about",
+//       element:<About/>
+//     },
+//     {
+//       path:"/contact",
+//       element:<Contact/>
+//     }
+//   ]
+//   }
+// ])
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}></Route>
+      <Route path='about' element={<About/>}></Route>
+      <Route path='contact' element={<Contact/>}></Route>
+
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
