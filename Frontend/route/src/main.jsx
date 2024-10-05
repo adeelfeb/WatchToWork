7ec77e7547e45f6f38594ec5aba8109daa01b803
adeelfeb <at4563323@gmail.com>
2024-10-05 19:08:34 +0500

    Ok router have a lot of thingis going on have to practice with lots of projects before finalizing the stack

--- Frontend/route/src/main.jsx
+++ Frontend/route/src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children:{
+      path:"",
+      element:<Home/>
+    }
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    
+    <RouterProvider router={router} />
   </StrictMode>,
 )
